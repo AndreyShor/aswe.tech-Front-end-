e86c61f8cbd364b9c66042e9f25939be262a3b67
Andrejs Shorstkin <andrejs232@gmail.com>
2019-09-25 22:16:02 +0300

    Add adaptive design for tablet in page About me, and add improve app functionality in edge browser

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,6 +58,9 @@
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
+
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
