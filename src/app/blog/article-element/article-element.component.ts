eb16451acc07cfbcb9fa364d9ce4a55e75e07461
Andrey <andrejs232@gmail.com>
2020-09-16 17:32:33 +0300

    Start fetchUser

--- src/app/blog/article-element/article-element.component.ts
+++ src/app/blog/article-element/article-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-element',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-element.component.scss']
 })
 export class ArticleElementComponent implements OnInit {
+   
+  @Input() title: string;
+  @Input() description: string;
+  @Input() imgUrl: string;
 
   constructor() { }
 
--- src/app/blog/article-list/article-list.component.ts
+++ src/app/blog/article-list/article-list.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleListComponent implements OnInit {
 
+  articleList: Array<{
+    title: string,
+    description: string,
+    imgUrl: string
+  }>
   constructor() { }
 
   ngOnInit() {
-  }
+    this.articleList = [{
+      title: "Hello world",
+      description: "Wrong do point avoid by fruit learn or in death. So passage however besides invited comfort elderly be me. Walls began of child civil am heard hoped my. Satisfied pretended mr on do determine by. Old post took and ask seen fact rich. Man entrance settling believed eat joy. Money as drift begin on to. Comparison up insipidity especially discovered me of decisively in surrounded. Points six way enough she its father. Folly sex downs tears ham green forty. ",
+      imgUrl: "/assets/img/blog/art-1.jpg"
+    },
+    {
+    title: "Hello world 2",
+      description: "aaaa Wrong do point avoid by fruit learn or in death. So passage however besides invited comfort elderly be me. Walls began of child civil am heard hoped my. Satisfied pretended mr on do determine by. Old post took and ask seen fact rich. Man entrance settling believed eat joy. Money as drift begin on to. Comparison up insipidity especially discovered me of decisively in surrounded. Points six way enough she its father. Folly sex downs tears ham green forty. ",
+      imgUrl: "/assets/img/blog/art-1.jpg"
+    },
+    {
+    title: "Hello world 3",
+      description: "bbbbb Wrong do point avoid by fruit learn or in death. So passage however besides invited comfort elderly be me. Walls began of child civil am heard hoped my. Satisfied pretended mr on do determine by. Old post took and ask seen fact rich. Man entrance settling believed eat joy. Money as drift begin on to. Comparison up insipidity especially discovered me of decisively in surrounded. Points six way enough she its father. Folly sex downs tears ham green forty. ",
+      imgUrl: "/assets/img/blog/art-1.jpg"
+    }
+  
+  
+  ]
+  }1
 
 }
